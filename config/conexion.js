657a7ede3af4cbe4bb115773877165f42afcb0e0
MitoCode <mitocode21@gmail.com>
2017-11-21 18:56:57 -0500

    mLab

--- config/conexion.js
+++ config/conexion.js
@@ -1,5 +1,5 @@
 let mongoose = require('mongoose');
 
-mongoose.connect('mongodb:mito:123@ds115866.mlab.com:15866/mitocrud', { useMongoClient: true }); //mongodb://localhost:27017/crud
+mongoose.connect('mongodb://mito:123@ds115866.mlab.com:15866/mitocrud', { useMongoClient: true }); //mongodb://localhost:27017/crud
 
 module.exports = mongoose;
